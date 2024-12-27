cb172645e92d602a3d878f32df5fb7051e9bfe1c
chans08 <chaalsgmll@gmail.com>
2024-12-28 08:09:03 +0900

    패딩수정

--- lib/app/root/ui/widget/greeting_view.dart
+++ lib/app/root/ui/widget/greeting_view.dart
@@ -12,7 +12,7 @@ class GreetingView extends StatelessWidget {
       child: Column(
         children: [
           Padding(
-            padding: edgeInsets(horizontal: 70, top: 70, bottom: 50),
+            padding: edgeInsets(horizontal: 60, top: 70, bottom: 50),
             child: R.image.banner.image(),
           ),
           Text(
